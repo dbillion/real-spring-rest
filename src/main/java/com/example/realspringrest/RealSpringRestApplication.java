d18d8a4ed0a59e124664d2a323a2941c031d2072
dbillion <dayozoe@gmail.com>
2023-01-25 14:54:18 +0200

    Initial commit

---
+++ src/main/java/com/example/realspringrest/RealSpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.realspringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealSpringRestApplication.class, args);
+	}
+
+}
